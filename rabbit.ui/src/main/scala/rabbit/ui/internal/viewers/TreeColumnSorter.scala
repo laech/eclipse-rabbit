1bb405e5e85ca329a621d97e92b3bbc20a938e8e
Lae <llaechen@gmail.com>
2011-12-27 20:11:52 +1300

    Updated documentation.

--- rabbit.ui/src/main/scala/rabbit/ui/internal/viewers/TreeColumnSorter.scala
+++ rabbit.ui/src/main/scala/rabbit/ui/internal/viewers/TreeColumnSorter.scala
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2011 The Rabbit Eclipse Plug-in Project
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package rabbit.ui.internal.viewers
 
 import org.eclipse.jface.viewers.TreeViewer
@@ -8,10 +23,25 @@ import org.eclipse.swt.events.SelectionListener
 import org.eclipse.swt.widgets.TreeColumn
 import org.eclipse.swt.SWT
 
+/** A `SelectionListener` that can be attached to a `TreeColumn` in responds to
+  * selection events of the column, then forwards sorting requests to a sortable
+  * object (usually a content provider) and updates the sorting indicator of the
+  * column, finally refreshes the viewer.
+  *
+  * @param viewer the parent `TreeViewer`
+  * @param sortable the object to delegate sorting requests to
+  * @param asc the natural ordering to pass to the sortable object when the
+  *      column is to be sorting ascendingly, descending sorting will be
+  *      handled by this instance
+  */
 final class TreeViewerColumnSorter(
   private val viewer: TreeViewer,
   private val sortable: Sortable,
   private val asc: Ordering[Any]) extends SelectionListener {
+
+  if (viewer == null) throw new NullPointerException("viewer")
+  if (sortable == null) throw new NullPointerException("sortable")
+  if (asc == null) throw new NullPointerException("asc")
 
   private val desc = new ReversedOrdering(asc)
   private val virtual = (viewer.getTree.getStyle & SWT.VIRTUAL) != 0
