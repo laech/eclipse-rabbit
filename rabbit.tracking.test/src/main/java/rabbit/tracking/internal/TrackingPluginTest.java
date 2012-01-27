f4739d70d3d610d3ee969b748b84daccfb64229b
Lae <llaechen@gmail.com>
2012-01-28 11:08:55 +1300

    Updated activator and tests.

---
+++ rabbit.tracking.test/src/main/java/rabbit/tracking/internal/TrackingPluginTest.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2010 The Rabbit Eclipse Plug-in Project
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package rabbit.tracking.internal;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.only;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+import java.lang.reflect.Field;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.osgi.framework.Bundle;
+
+import rabbit.tracking.ITracker;
+
+import com.google.common.collect.Sets;
+
+public final class TrackingPluginTest {
+
+  private static final Bundle bundle = TrackingPlugin.getDefault().getBundle();
+
+  private TrackingPlugin plugin;
+
+  @Before public void initPlugin() throws Exception {
+    plugin = TrackingPlugin.getDefault();
+  }
+
+  @After public void resetPlugin() throws Exception {
+    bundle.stop();
+    bundle.start();
+  }
+
+  @Test public void shouldDisableTrackerOnStop() throws Exception {
+    ITracker tracker = mock(ITracker.class);
+    setTrackers(plugin, tracker);
+    bundle.stop();
+    verify(tracker).setEnabled(false);
+    verify(tracker).saveData();
+  }
+
+  @Test public void shouldDisableTrackersOnShutdown() throws Exception {
+    ITracker tracker = mock(ITracker.class);
+    setTrackers(plugin, tracker);
+    assertThat(plugin.preShutdown(null, false), is(true));
+    verify(tracker).setEnabled(false);
+    verify(tracker).saveData();
+    verifyNoMoreInteractions(tracker);
+  }
+
+  @Test public void shouldSaveCurrentDataWhenAsked() throws Exception {
+    ITracker tracker = mock(ITracker.class);
+    setTrackers(plugin, tracker);
+    plugin.saveCurrentData();
+    verify(tracker, only()).saveData();
+  }
+
+  private void setTrackers(TrackingPlugin plugin, ITracker... trackers) {
+    try {
+      Field f = TrackingPlugin.class.getDeclaredField("trackers");
+      f.setAccessible(true);
+      f.set(plugin, Sets.newHashSet(trackers));
+    } catch (Exception e) {
+      Assert.fail(e.getMessage());
+    }
+  }
+}
---
+++ rabbit.tracking/src/main/java/rabbit/tracking/internal/TrackingPlugin.java
@@ -0,0 +1,116 @@
+/*
+ * Copyright 2010 The Rabbit Eclipse Plug-in Project
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package rabbit.tracking.internal;
+
+import static java.util.Collections.emptySet;
+import static org.eclipse.core.runtime.Platform.getExtensionRegistry;
+import static org.eclipse.core.runtime.SafeRunner.run;
+
+import java.util.Collection;
+import java.util.Set;
+
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.jface.util.SafeRunnable;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchListener;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+import rabbit.tracking.ITracker;
+
+import com.google.common.collect.ImmutableSet;
+
+public final class TrackingPlugin extends AbstractUIPlugin
+    implements IWorkbenchListener {
+
+  private static TrackingPlugin plugin;
+
+  public static TrackingPlugin getDefault() {
+    return plugin;
+  }
+
+  private volatile Set<ITracker> trackers;
+
+  public TrackingPlugin() {
+    trackers = emptySet();
+  }
+
+  @Override public void postShutdown(IWorkbench workbench) {
+    // Everything should be done before the workbench is shut down, use
+    // preShutdown method instead
+  }
+
+  @Override public boolean preShutdown(IWorkbench workbench, boolean forced) {
+    setEnable(trackers, false);
+    saveCurrentData();
+    return true;
+  }
+
+  /**
+   * Call this method to saves all current data collected by the trackers now.
+   */
+  public void saveCurrentData() {
+    for (ITracker tracker : trackers) {
+      tracker.saveData();
+    }
+  }
+
+  @Override public void start(BundleContext context) throws Exception {
+    super.start(context);
+    plugin = this;
+    trackers = loadTrackers();
+    setEnable(trackers, true);
+    getWorkbench().addWorkbenchListener(this);
+  }
+
+  @Override public void stop(BundleContext context) throws Exception {
+    try {
+      getWorkbench().removeWorkbenchListener(this);
+      setEnable(trackers, false);
+      saveCurrentData();
+      trackers = emptySet();
+      plugin = null;
+    } finally {
+      super.stop(context);
+    }
+  }
+
+  private Set<ITracker> loadTrackers() {
+    IConfigurationElement[] elements = getExtensionRegistry()
+        .getConfigurationElementsFor("rabbit.tracking", "trackers");
+
+    final ImmutableSet.Builder<ITracker> builder = ImmutableSet.builder();
+    for (final IConfigurationElement element : elements) {
+      run(new SafeRunnable() {
+        @Override public void run() throws Exception {
+          Object o = element.createExecutableExtension("class");
+          if (o instanceof ITracker) {
+            builder.add((ITracker)o);
+          } else {
+            throw new IllegalArgumentException("Not a tracker: " + o);
+          }
+        }
+      });
+    }
+    return builder.build();
+  }
+
+  private void setEnable(Collection<ITracker> trackers, boolean enable) {
+    for (ITracker tracker : trackers) {
+      tracker.setEnabled(enable);
+    }
+  }
+}
