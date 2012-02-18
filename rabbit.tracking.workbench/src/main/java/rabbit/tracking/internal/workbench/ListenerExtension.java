b1c86f6f03884c824a67aec79be7f181fba7ac22
Lae <llaechen@gmail.com>
2012-02-18 23:12:56 +1300

    Listener extension helpers and tests.

---
+++ rabbit.tracking.workbench/src/main/java/rabbit/tracking/internal/workbench/ListenerExtension.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright 2012 The Rabbit Eclipse Plug-in Project
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package rabbit.tracking.internal.workbench;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import java.util.List;
+import java.util.Set;
+
+import org.eclipse.core.runtime.IConfigurationElement;
+
+import rabbit.tracking.ITrackerEventListener;
+
+import com.google.common.collect.ImmutableCollection;
+import com.google.common.collect.ImmutableSet;
+
+/**
+ * Helper class for loading extensions from
+ * {@code rabbit.tracking.workbench.listeners}.
+ */
+final class ListenerExtension<T> {
+
+  /**
+   * Creates a new instance.
+   * 
+   * @param tag the tag name to match elements when loading
+   * @param clazz the expected type or super type of the loaded object
+   * @return a new instance, not null
+   * @throws NullPointerException if any argument is null
+   */
+  public static <T extends ITrackerEventListener<?>> ListenerExtension<T> extension(
+      String tag, Class<T> clazz) {
+    return new ListenerExtension<T>(tag, clazz);
+  }
+
+  private final String tag;
+  private final Class<T> clazz;
+
+  private ListenerExtension(String tag, Class<T> clazz) {
+    this.tag = checkNotNull(tag, "tag");
+    this.clazz = checkNotNull(clazz, "class");
+  }
+
+  /**
+   * Loads all the objects from the configuration elements that matches the tag
+   * and class configured in this extension.
+   * 
+   * @param elements the elements to find and create objects from
+   * @param errorsBucket to put errors into, if any
+   * @return the loaded objects, not null, may be empty, unmodifiable
+   * @throws NullPointerException if any argument is null
+   */
+  public Set<T> load(IConfigurationElement[] elements,
+      List<Exception> errorsBucket) {
+    checkNotNull(elements, "elements");
+    checkNotNull(errorsBucket, "errorsBucket");
+
+    ImmutableSet.Builder<T> builder = ImmutableSet.builder();
+    for (IConfigurationElement element : elements) {
+      try {
+        if (getListenerTag().equalsIgnoreCase(element.getName())) {
+          load(element, errorsBucket, builder);
+        }
+      } catch (Exception e) {
+        errorsBucket.add(e);
+      }
+    }
+    return builder.build();
+  }
+
+  @SuppressWarnings("unchecked")//
+  private void load(IConfigurationElement element, List<Exception> errors,
+      ImmutableCollection.Builder<T> builder) throws Exception {
+
+    Object obj = element.createExecutableExtension("class");
+    if (getListenerClass().isAssignableFrom(obj.getClass())) {
+      builder.add((T)obj);
+    } else {
+      throw new IllegalArgumentException("Not a "
+          + getListenerClass().getSimpleName() + ": " + obj);
+    }
+  }
+
+  private Class<T> getListenerClass() {
+    return clazz;
+  }
+
+  private String getListenerTag() {
+    return tag;
+  }
+}
--- rabbit.tracking.workbench/src/main/java/rabbit/tracking/internal/workbench/WorkbenchPlugin.java
+++ rabbit.tracking.workbench/src/main/java/rabbit/tracking/internal/workbench/WorkbenchPlugin.java
@@ -16,13 +16,18 @@
 
 package rabbit.tracking.internal.workbench;
 
+import static com.google.common.collect.Lists.newArrayList;
+import static java.util.Collections.emptySet;
+import static org.eclipse.core.runtime.IStatus.ERROR;
 import static org.eclipse.core.runtime.Platform.getExtensionRegistry;
-import static org.eclipse.core.runtime.SafeRunner.run;
+import static rabbit.tracking.internal.workbench.ListenerExtension.extension;
 
+import java.util.List;
 import java.util.Set;
 
 import org.eclipse.core.runtime.IConfigurationElement;
-import org.eclipse.jface.util.SafeRunnable;
+import org.eclipse.core.runtime.IExtensionRegistry;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
@@ -35,53 +40,105 @@ import rabbit.tracking.workbench.ISessionEventListener;
 
 public final class WorkbenchPlugin extends AbstractUIPlugin {
 
-  private static WorkbenchPlugin plugin;
+  private static volatile WorkbenchPlugin plugin;
 
   public static WorkbenchPlugin getDefault() {
     return plugin;
   }
 
+  private volatile Set<ICommandEventListener> commandListeners;
+  private volatile Set<IFileEventListener> fileListeners;
+  private volatile Set<ILaunchEventListener> launchListeners;
+  private volatile Set<IPartEventListener> partListeners;
+  private volatile Set<IPerspectiveEventListener> perspectiveListeners;
+  private volatile Set<ISessionEventListener> sessionListeners;
+
   public WorkbenchPlugin() {
+    resetListeners();
   }
 
   @Override public void start(BundleContext context) throws Exception {
     super.start(context);
     plugin = this;
+    loadListeners(getExtensionRegistry());
   }
 
   @Override public void stop(BundleContext context) throws Exception {
     try {
       plugin = null;
+      resetListeners();
     } finally {
       super.stop(context);
     }
   }
 
-  private <T> Set<T> loadListeners(Class<T> listenerClass) {
-    IConfigurationElement[] elements = getExtensionRegistry()
-        .getConfigurationElementsFor("rabbit.tracking.workbench", "listeners");
+  void resetListeners() {
+    commandListeners = emptySet();
+    fileListeners = emptySet();
+    launchListeners = emptySet();
+    partListeners = emptySet();
+    perspectiveListeners = emptySet();
+    sessionListeners = emptySet();
+  }
 
-    for (final IConfigurationElement element : elements) {
-      run(new SafeRunnable() {
-        @Override public void run() throws Exception {
-          Object obj = element.createExecutableExtension("class");
-          if (obj instanceof ICommandEventListener) {
-            
-          } else if (obj instanceof IFileEventListener) {
-            
-          } else if (obj instanceof ILaunchEventListener) {
-            
-          } else if (obj instanceof IPartEventListener) {
-            
-          } else if (obj instanceof IPerspectiveEventListener) {
-            
-          } else if (obj instanceof ISessionEventListener) {
-            
-          } else {
-            throw new IllegalArgumentException(); // TODO
-          }
-        }
-      });
+  void loadListeners(IExtensionRegistry registry) {
+    String pluginId = getBundle().getSymbolicName();
+
+    IConfigurationElement[] elements = registry
+        .getConfigurationElementsFor(pluginId, "listeners");
+
+    List<Exception> errorsBucket = newArrayList();
+
+    commandListeners =
+        extension("commandEventListener", ICommandEventListener.class)
+            .load(elements, errorsBucket);
+
+    fileListeners =
+        extension("fileEventListener", IFileEventListener.class)
+            .load(elements, errorsBucket);
+
+    launchListeners =
+        extension("launchEventListener", ILaunchEventListener.class)
+            .load(elements, errorsBucket);
+
+    partListeners =
+        extension("partEventListener", IPartEventListener.class)
+            .load(elements, errorsBucket);
+
+    perspectiveListeners =
+        extension("perspectiveEventListener", IPerspectiveEventListener.class)
+            .load(elements, errorsBucket);
+
+    sessionListeners =
+        extension("sessionEventListener", ISessionEventListener.class)
+            .load(elements, errorsBucket);
+
+    for (Exception e : errorsBucket) {
+      getLog().log(new Status(ERROR, pluginId, e.getMessage(), e));
     }
+  }
+
+  public Set<ICommandEventListener> getCommandEventListeners() {
+    return commandListeners;
+  }
+
+  public Set<IFileEventListener> getFileEventListeners() {
+    return fileListeners;
+  }
+
+  public Set<ILaunchEventListener> getLaunchEventListeners() {
+    return launchListeners;
+  }
+
+  public Set<IPartEventListener> getPartEventListeners() {
+    return partListeners;
+  }
+
+  public Set<IPerspectiveEventListener> getPerspectiveEventListeners() {
+    return perspectiveListeners;
+  }
+
+  public Set<ISessionEventListener> getSessionEventListeners() {
+    return sessionListeners;
   }
 }
