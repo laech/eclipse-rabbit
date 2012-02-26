1c547bedc2fee01768927ab739957550a6623ef5
Lae <llaechen@gmail.com>
2012-02-26 21:03:47 +1300

    Added part tracker to be composed, working on removing tracker inheritance.

---
+++ rabbit.tracking.workbench.test/src/main/scala/rabbit/tracking/workbench/PartTrackerSpec.scala
@@ -0,0 +1,182 @@
+/*
+ * Copyright 2012 The Rabbit Eclipse Plug-in Project
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package rabbit.tracking.workbench
+
+import org.eclipse.ui.{ IWorkbenchWindow, IWorkbenchPart }
+import org.junit.runner.RunWith
+import org.mockito.Matchers.any
+import org.mockito.Mockito.{ verifyZeroInteractions, verify, only, never, inOrder, atLeastOnce }
+import org.scalatest.mock.MockitoSugar.mock
+import org.scalatest.junit.JUnitRunner
+
+import rabbit.tracking.workbench.PartTracker.IListener
+import rabbit.tracking.workbench.test.WorkbenchTestUtil.{ openWindow, openRandomPart, hide, closeAllParts, close, activate }
+import rabbit.tracking.AbstractTrackerSpecBase
+
+@RunWith(classOf[JUnitRunner])
+final class PartTrackerSpec extends AbstractTrackerSpecBase {
+
+  private[this] var window: IWorkbenchWindow = _
+
+  override def beforeEach() {
+    super.beforeEach()
+    listener = mock[IListener]
+    tracker.addListener(listener)
+    closeAllParts();
+  }
+
+  override def afterEach() {
+    super.afterEach()
+    if (window != null) {
+      close(window)
+      window = null
+    }
+  }
+
+  type Tracker = PartTracker
+
+  private var listener: IListener = _
+
+  behavior of "PartTracker"
+
+  it must "notify part focused when a part is already focused when enabled" in {
+    val part = activate(openRandomPart())
+    tracker.enable()
+    verify(listener, only).onPartFocused(part)
+  }
+
+  it must "notify part focused due to part opened" in {
+    tracker.enable()
+    val part = openRandomPart()
+    verify(listener).onPartFocused(part)
+  }
+
+  it must "notify part focused due to part selected" in {
+    val part1 = openRandomPart()
+    val part2 = openRandomPart()
+    activate(part1)
+    tracker.enable()
+
+    activate(part2)
+    verify(listener).onPartFocused(part2)
+  }
+
+  it must "notify part unfocused due to new part selected" in {
+    val part1 = openRandomPart()
+    val part2 = openRandomPart()
+    activate(part1)
+    tracker.enable()
+
+    activate(part2)
+    verify(listener).onPartUnfocused(part1)
+  }
+
+  it must "notify part unfocused due to new part opened" in {
+    tracker.enable()
+    val part = openRandomPart()
+
+    openRandomPart()
+    verify(listener).onPartUnfocused(part)
+  }
+
+  it must "notify part unfocused due to window unfocused" in {
+    tracker.enable()
+    val part = openRandomPart()
+
+    window = openWindow()
+    verify(listener).onPartUnfocused(part)
+  }
+
+  it must "notify part unfocused due to part closed" in {
+    val part = openRandomPart();
+    tracker.enable()
+
+    hide(part)
+    verify(listener).onPartUnfocused(part)
+  }
+
+  it must "notify part unfocused due to window closed" in {
+    val window = openWindow()
+    var part: IWorkbenchPart = null
+    try {
+      part = openRandomPart(window)
+      tracker.enable()
+      verify(listener, never).onPartUnfocused(any[IWorkbenchPart])
+    } finally {
+      close(window)
+    }
+    verify(listener).onPartUnfocused(part)
+  }
+
+  it must "notify part unforcused on old part before notifying part focused on new part" in {
+    tracker.enable()
+    openRandomPart()
+    openRandomPart()
+
+    val order = inOrder(listener)
+    order.verify(listener).onPartUnfocused(any[IWorkbenchPart])
+    order.verify(listener).onPartFocused(any[IWorkbenchPart])
+    order.verifyNoMoreInteractions()
+  }
+
+  it must "not notify when disabled" in {
+    tracker.disable()
+    openRandomPart()
+    openRandomPart()
+    verifyZeroInteractions(listener)
+  }
+
+  it must "track newly opened window" in {
+    tracker.enable()
+    window = openWindow()
+    openRandomPart(window)
+    verify(listener, atLeastOnce).onPartFocused(any[IWorkbenchPart])
+  }
+
+  it must "add listener when asked" in {
+    tracker.enable()
+    val listener = mock[IListener]
+    tracker.addListener(listener)
+    openRandomPart()
+    verify(listener).onPartFocused(any[IWorkbenchPart])
+  }
+
+  it must "remove listener when asked" in {
+    tracker.enable()
+    closeAllParts()
+    val listener = mock[IListener]
+    tracker.addListener(listener)
+
+    tracker.removeListener(listener)
+    openRandomPart()
+    verifyZeroInteractions(listener)
+  }
+
+  it must "throw NullPointerException if listener to add is null" in {
+    intercept[NullPointerException] {
+      tracker.addListener(null)
+    }
+  }
+
+  it must "throw NullPointerException if listener to remove is null" in {
+    intercept[NullPointerException] {
+      tracker.removeListener(null)
+    }
+  }
+
+  override protected def create() = new PartTracker
+}
