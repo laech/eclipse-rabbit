ac3860bd26d900690be11ebd6ea245b24d83602b
Lae <llaechen@gmail.com>
2012-05-01 21:41:45 +1200

    Part tracker.

--- rabbit.workbench.tracking/src/main/java/rabbit/workbench/internal/tracking/PartTracker.java
+++ rabbit.workbench.tracking/src/main/java/rabbit/workbench/internal/tracking/PartTracker.java
@@ -1,38 +1,58 @@
+/*
+ * Copyright 2012 The Rabbit Eclipse Plug-in Project
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package rabbit.workbench.internal.tracking;
-///*
-// * Copyright 2010 The Rabbit Eclipse Plug-in Project
-// * 
-// * Licensed under the Apache License, Version 2.0 (the "License"); you may not
-// * use this file except in compliance with the License. You may obtain a copy of
-// * the License at
-// * 
-// * http://www.apache.org/licenses/LICENSE-2.0
-// * 
-// * Unless required by applicable law or agreed to in writing, software
-// * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-// * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-// * License for the specific language governing permissions and limitations under
-// * the License.
-// */
-//package rabbit.tracking.internal.workbench;
-//
-//import org.eclipse.ui.IWorkbenchPart;
-//
-//import rabbit.tracking.AbstractPartTracker;
-//
-//public final class PartTracker extends AbstractPartTracker {
-//
-//  public PartTracker() {
-//    super();
-//  }
-//
-//  @Override public void save() {
-//    // TODO Auto-generated method stub
-//
-//  }
-//
-//  @Override protected void onPartEvent(long start, long end, IWorkbenchPart part) {
-//    // TODO Auto-generated method stub
-//
-//  }
-// }
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import org.eclipse.ui.IWorkbenchPart;
+import org.joda.time.Duration;
+import org.joda.time.Instant;
+
+import rabbit.tracking.IListenableTracker;
+import rabbit.tracking.IPartSessionListener;
+import rabbit.tracking.IPersistable;
+import rabbit.tracking.util.IPersistableEventListenerSupport;
+
+import com.google.inject.Inject;
+
+public final class PartTracker extends AbstractPartTracker
+    implements IPersistable {
+
+  private final IPersistableEventListenerSupport<IPartEvent> support;
+
+  /**
+   * @param tracker the tracker to use for listening to part session events
+   * @param support the listener support for sending events
+   * @throws NullPointerException if any argument is null
+   */
+  @Inject public PartTracker(
+      IListenableTracker<IPartSessionListener> tracker,
+      IPersistableEventListenerSupport<IPartEvent> support) {
+    super(tracker);
+    this.support = checkNotNull(support, "support");
+  }
+
+  @Override protected void onPartSession(
+      Instant start, Duration duration, IWorkbenchPart part) {
+    support.notifyOnEvent(new PartEvent(start, duration, part));
+  }
+
+  @Override public void save() {
+    support.notifyOnSave();
+  }
+
+}
