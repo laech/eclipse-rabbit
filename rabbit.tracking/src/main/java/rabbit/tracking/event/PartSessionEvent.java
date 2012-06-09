2aa919e683ca17dba89f0104edc143c6b3e2b864
Lae <llaechen@gmail.com>
2012-06-09 22:41:47 +1200

    Added PartSessionEvent

--- rabbit.tracking/src/main/java/rabbit/tracking/event/PartSessionEvent.java
+++ rabbit.tracking/src/main/java/rabbit/tracking/event/PartSessionEvent.java
@@ -16,71 +16,61 @@
 
 package rabbit.tracking.event;
 
+import static com.google.common.base.Objects.equal;
 import static com.google.common.base.Preconditions.checkNotNull;
 
 import org.eclipse.ui.IWorkbenchPart;
+import org.joda.time.Duration;
 import org.joda.time.Instant;
 
 import com.google.common.base.Objects;
 import com.google.common.base.Objects.ToStringHelper;
 
 /**
- * A workbench part focus/unfocus event. A part is consider focused if all of
- * the followings are true:
- * <ul>
- * <li>it's the active part in the parent window</li>
- * <li>its parent window is focused</li>
- * </ul>
- * therefore there will be at most one focused part at any time regardless of
- * how many workbench windows are opened.
+ * Part focused duration event.
+ * <p/>
+ * If a part is focused, then when it becomes unfocused (or by some other means
+ * such as the user becomes inactive), an event is generated.
+ * <p/>
+ * A part is consider focused if it's the active part and its parent window has
+ * the focus.
  * 
  * @since 2.0
  */
-public final class PartFocusEvent extends Event {
+public final class PartSessionEvent extends TimedEvent {
 
-  private final boolean focused;
   private final IWorkbenchPart part;
 
   /**
    * Constructs a new event.
    * 
-   * @param instant the instant of this event
+   * @param instant the instant (start time) of this event
+   * @param duration the duration of this event
    * @param part the workbench part of this event
-   * @param focused true if the part became focused, false if the part became
-   *        unfocused
    * @throws NullPointerException if any argument is null
    */
-  public PartFocusEvent(Instant instant, IWorkbenchPart part, boolean focused) {
-    super(instant);
+  public PartSessionEvent(
+      Instant instant, Duration duration, IWorkbenchPart part) {
+    super(instant, duration);
     this.part = checkNotNull(part, "part");
-    this.focused = focused;
   }
 
   @Override public boolean equals(Object obj) {
-    if (obj instanceof PartFocusEvent) {
-      PartFocusEvent that = (PartFocusEvent)obj;
-      return Objects.equal(instant(), that.instant())
-          & Objects.equal(part(), that.part())
-          & Objects.equal(isFocused(), that.isFocused());
+    if (obj instanceof PartSessionEvent) {
+      PartSessionEvent that = (PartSessionEvent)obj;
+      return equal(instant(), that.instant())
+          && equal(duration(), that.duration())
+          && equal(part(), that.part());
     }
     return false;
   }
 
   @Override public int hashCode() {
-    return Objects.hashCode(instant(), part(), isFocused());
+    return Objects.hashCode(instant(), duration(), part());
   }
 
   /**
-   * The focus state of the workbench part of this event.
-   * 
-   * @return true if the part was focused, false if it was unfocused
-   */
-  public boolean isFocused() {
-    return focused;
-  }
-
-  /**
-   * The workbench part of this event
+   * The workbench part of this event.
    * 
    * @return the workbench part, not null
    */
@@ -89,9 +79,6 @@ public final class PartFocusEvent extends Event {
   }
 
   @Override protected ToStringHelper toStringHelper() {
-    return super.toStringHelper()
-        .add("part", part())
-        .add("focused", isFocused());
+    return super.toStringHelper().add("part", part());
   }
-
 }
